014f339692f67a245d82376affeb46b03297da23
RHenintsoa <henintsoarasolofohery@gmail.com>
2023-09-23 23:13:28 +0300

    interface form

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="./assets/img/favicon.png">
+	<!-- css-->
+	<link rel="stylesheet" type="text/css" href="./assets/bootstrap/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	
+	<!-- bootstrap icons files-->
+	<link rel="stylesheet" type="text/css" href="./assets/bootstrap/bootstrap-icons/bootstrap-icons.css">
+
+	<title>Inscription</title>
+</head>
+
+<body>
+	<header>
+
+		<nav class="navbar navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-brand">
+					<span>Welcome</span>
+				</div>
+			</div>
+
+		</nav>
+	</header>
+	<div class="container content">
+		<div class="row test">
+			<div class="col-lg-6">
+				<img src="./assets/img/img2.jpg" alt="" class="img-fluid" id="picture">
+			</div>
+			<div class="col-lg-4">
+				<form action="./list.php"></form>
+			</div>
+		</div>
+	</div>
+	
+<!-- script-->
+	<script type="text/javascript" src="./assets/js/jquery.js"></script>
+	<script type="text/javascript" src="./assets/js/index.js"></script>
+	<script type="text/javascript" src="./assets/bootstrap/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
